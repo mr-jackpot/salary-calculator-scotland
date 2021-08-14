45040c4344a24f76d73327cd6b73d6e9180ae457
Adam Heeps <adamheeps@adams-mbp.home>
2021-08-14 22:32:58 +0100

    Added hourly rate calculations

--- src/main/java/com/adam/calculators/salarycalculator/App.java
+++ src/main/java/com/adam/calculators/salarycalculator/App.java
@@ -3,6 +3,7 @@ package com.adam.calculators.salarycalculator;
 public class App {
 
     public static void main(String[] args) {
-        User u = new User("Adam", (short) 22, 0d, 7.0d);
+        User u = new User("Adam", (short) 22, 100000.0d, 7.0d);
+        CalculationProcessor calc = new CalculationProcessor(u, 0);
     }
 }
